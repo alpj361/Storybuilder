1b25cff460952d1a249c766312abea5ffb9bc190
pj <alpj361@gmail.com>
2025-11-16 17:02:27 -0600

    Trying to get .env work correctly

---
+++ app.config.js
@@ -0,0 +1,38 @@
+// Load environment variables from .env file
+// This is required for Expo to access EXPO_PUBLIC_* variables at runtime
+require('dotenv').config();
+
+module.exports = {
+  expo: {
+    name: "vibecode",
+    slug: "vibecode",
+    scheme: "vibecode",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.alpj361.vibecode"
+    },
+    android: {
+      edgeToEdgeEnabled: true,
+      package: "com.alpj361.vibecode"
+    },
+    plugins: [
+      "expo-asset",
+      "expo-build-properties",
+      "expo-font",
+      "expo-secure-store",
+      "expo-video",
+      "expo-mail-composer",
+      "expo-sqlite",
+      "expo-web-browser"
+    ],
+    // Pass environment variables to the app at runtime
+    extra: {
+      EXPO_PUBLIC_OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
+      EXPO_PUBLIC_REPLICATE_API_KEY: process.env.EXPO_PUBLIC_REPLICATE_API_KEY,
+    }
+  }
+};
